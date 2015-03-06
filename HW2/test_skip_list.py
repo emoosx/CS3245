98d4f6ef40c2f6b552c12f7f67a01ac3fbf5dec5
Kaung Htet Zaw <emoosx@gmail.com>
2015-03-06 10:35:47 +0800

    Correctly build skip node pairs

--- HW2/test_skip_list.py
+++ HW2/test_skip_list.py
@@ -13,10 +13,6 @@ class TestSkipList:
         eq_(len(self.skip_list), 1)
         eq_(self.skip_list.root, self.skip_list.last)
 
-    def test_print_node(self):
-        self.skip_list.append(3)
-        eq_(str(self.skip_list.root), str(3))
-
 
     def test_append(self):
         self.skip_list.append(1)
@@ -26,3 +22,20 @@ class TestSkipList:
         eq_(self.skip_list.root.val(), 1)
         eq_(len(self.skip_list), 4)
         eq_(self.skip_list.last.val(), 4)
+
+
+    def test_generate_skips(self):
+        for i in range(30):
+            self.skip_list.append(i)
+        skip_pairs = [(a, b) for (a, b) in self.skip_list.generate_skips()]
+        eq_(skip_pairs, [(1, 6), (6, 11), (11, 16), (16, 21), (21, 26)])
+
+
+    # def test_create_skip_pointers(self):
+    #     for i in range(30):
+    #         self.skip_list.append(i)
+    #     self.skip_list.create_skip_pointers()
+    #     eq_(1, 2)
+            
+        
+        
